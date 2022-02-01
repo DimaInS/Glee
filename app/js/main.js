45a4c3250b43905877d60c20464256b6e4078747
DimaLnS <dmadamchuk@gmail.com>
2022-02-01 10:02:28 +0200

    01.02

--- app/js/main.js
+++ app/js/main.js
@@ -3,5 +3,15 @@ $(function() {
     arrows: false,
     dots:true
  });
- var mixer = mixitup('.products__content');
+ var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+ 
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+ 
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
 });
