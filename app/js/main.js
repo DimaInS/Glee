0fada91f3450d9ea519ee0f624ee53f94fe542bb
DimaLnS <dmadamchuk@gmail.com>
2022-02-06 17:35:20 +0200

    added filters for productlist

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,27 @@
 $(function() {
+  $(".star").rateYo({
+    readOnly: true,
+    starWidth: "12px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    spacing: "8px"
+  });
+
+ $(".filter-price__input").ionRangeSlider({
+        type: "double",
+         prefix: "$",
+         onStart: function (data) {
+           $('.filter-price__from').text(data.from);
+           $('.filter-price__to').text(data.to);
+         },
+         onChange: function (data) {
+           $('.filter-price__from').text(data.from);
+           $('.filter-price__to').text(data.to);
+         },
+       
+    });
+
+
  $('.top-slider__top').slick({
     arrows: false,
     dots:true
